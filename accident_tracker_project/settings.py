545c83f40e1955da3de936462fa04cb2d92bde6e
Rayhan Shikder <rayhanshikder@Rayhans-MacBook-Pro-2.local>
2018-05-29 01:04:28 -0400

    added mysqlclient

--- accident_tracker_project/settings.py
+++ accident_tracker_project/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'accident_tracker_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_db',
+        'USER': 'shiku',
+        'PASSWORD': 'shiku',
+        'HOST': '',
+        'PORT': '', 
+
     }
 }
 
